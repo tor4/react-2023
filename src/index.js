cb952af8ff330365bdf284ffe410e14a89cf9c24
Nataly Zarubina <nataly.zarubina@gmail.com>
2023-10-17 14:21:23 +0200

    Setup Router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import {
+  RouterProvider,
+} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+import { router } from './router';
 import './setup.js';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
